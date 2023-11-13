884c2ae2782e1a9cff8b1851146272b5662a9687
RitvikSardana <ritvik.sardana11@gmail.com>
2023-11-13 13:08:31 +0530

    chore: status change

--- education/education/doctype/student_applicant/student_applicant.js
+++ education/education/doctype/student_applicant/student_applicant.js
@@ -37,8 +37,8 @@ frappe.ui.form.on("Student Applicant", {
 		}
 
 		if (!frm.is_new() && frm.doc.application_status === "Rejected") {
-			frm.add_custom_button(__("Enroll"), function() {
-				frm.set_value("application_status", "Applied");
+			frm.add_custom_button(__("Approve"), function() {
+				frm.set_value("application_status", "Approved");
 				frm.save_or_update();
 			}, 'Actions');
 		}
