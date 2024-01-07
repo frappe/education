d94bdc23a60760db82dc7385401cfb97ba7a257d
RitvikSardana <ritvik.sardana11@gmail.com>
2024-01-07 12:22:19 +0530

    fix: create item master when a fee component is created

--- education/education/doctype/fee_category/fee_category.py
+++ education/education/doctype/fee_category/fee_category.py
@@ -6,11 +6,29 @@ from frappe.model.document import Document
 
 
 class FeeCategory(Document):
+	def on_update(self):
+		# update item
+		update_item(self)
+
 	def after_insert(self):
 		# create an item
-		item_master = frappe.new_doc("Item")
-		item_master.item_code = self.name
-		item_master.item_name = self.name
-		# TODO: After installation create an item group called Fee Component
-		item_master.item_group = "Fee Component"
-		item_master.insert()
+		create_item(self)
+
+
+def create_item(doc):
+	item_master = frappe.new_doc("Item")
+	item_master.item_code = doc.name
+	item_master.description = doc.description
+	# TODO: After installation create an item group called Fee Component
+	item_master.item_group = "Fee Component"
+	item_master.is_sales_item = 1
+	item_master.is_service_item = 1
+
+	item_master.insert()
+
+
+def update_item(doc):
+	item = frappe.get_doc("Item", doc.name)
+	item.item_name = doc.name
+	item.description = doc.description
+	item.save()
