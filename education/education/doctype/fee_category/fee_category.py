a2ed224f6da5fb63d3d4881e5e649caacfee98ea
RitvikSardana <ritvik.sardana11@gmail.com>
2024-01-07 11:57:00 +0530

    fix: create sales invoice item when a fee category is created

--- education/education/doctype/fee_category/fee_category.py
+++ education/education/doctype/fee_category/fee_category.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2015, Frappe Technologies and contributors
 # For license information, please see license.txt
 
-
+import frappe
 from frappe.model.document import Document
 
 
 class FeeCategory(Document):
-	pass
+	def after_insert(self):
+		# create an item
+		item_master = frappe.new_doc("Item")
+		item_master.item_code = self.name
+		item_master.item_name = self.name
+		# TODO: After installation create an item group called Fee Component
+		item_master.item_group = "Fee Component"
+		item_master.insert()
