51eb84ec71e5d04d2d4973b1003a7759e1c29062
RitvikSardana <ritvik.sardana11@gmail.com>
2024-02-22 15:59:07 +0530

    fix: change copy file in www

--- education/public/frontend/assets/Attendance.e3077277.js
+++
@@ -1,1 +0,0 @@
-import{G as e,q as n,B as t}from"./frappe-ui.49d8c9fc.js";const c={};function r(a,o){return n(),t("div",null," Attendance ")}var _=e(c,[["render",r]]);export{_ as default};
--- education/public/frontend/assets/Fees.34bdecc8.js
+++
@@ -1,1 +0,0 @@
-import{G as e,q as r,B as s}from"./frappe-ui.49d8c9fc.js";const t={};function o(a,c,n,p,_,f){return r(),s("div",null," Fees ")}var d=e(t,[["render",o]]);export{d as default};
