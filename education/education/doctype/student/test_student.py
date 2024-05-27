397030de03c6a4b776d0f52ff53fe946280a67fc
RitvikSardana <ritvik.sardana11@gmail.com>
2024-05-27 13:26:31 +0530

    test: add test for checking customer group

--- education/education/doctype/student/test_student.py
+++ education/education/doctype/student/test_student.py
@@ -23,6 +23,7 @@ class TestStudent(FrappeTestCase):
 	def test_create_customer_against_student(self):
 		student = frappe.get_doc("Student", {"student_email_id": "test@example.com"})
 		self.assertTrue(bool(student.customer))
+		self.assertEqual(student.customer_group, "Student")
 
 	def tearDown(self):
 		frappe.db.rollback()
