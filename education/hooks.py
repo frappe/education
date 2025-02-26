76c8524e7f31a72bb316fd4073118bd34eef43b9
Dinie Masjuki <diniemasjuki@users.noreply.github.com>
2025-02-26 11:10:02 +0800

    Update hooks.py

--- education/hooks.py
+++ education/hooks.py
@@ -104,12 +104,6 @@ global_search_doctypes = {
 }
 
 # fixed route to education setup
-hooks = {
-    "domains": ["education"],
-    "domains": {
-        "education": "education.education.setup"
-    }
-}
 domains = {
 	"Education": "education.education.setup"
 }
