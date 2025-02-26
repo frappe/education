f767226439a6f51852d5c7368860f018eb427949
Dinie Masjuki <diniemasjuki@users.noreply.github.com>
2025-02-26 11:30:14 +0800

    Update setup.py

--- education/education/setup.py
+++ education/education/setup.py
@@ -1,11 +1,19 @@
-# education/education/setup.py
-from frappe import _
+from setuptools import setup, find_packages
 
-def get_data():
-    return {
-        "fieldname": "school",
-        "transactions": [
-            {"label": _("Student"), "items": ["Student", "Student Group"]},
-            {"label": _("Fee"), "items": ["Fees", "Fee Structure"]},
-        ],
-    }
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
+
+# get version from __version__ variable in education/__init__.py
+from education import __version__ as version
+
+setup(
+	name="education",
+	version=version,
+	description="Education",
+	author="Frappe Technologies Pvt. Ltd.",
+	author_email="hello@frappe.io",
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires,
+)
