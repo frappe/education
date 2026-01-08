258158b0ef19acd6c527e1329104c6df0477d720
John Longland <152137219+johnwslongland@users.noreply.github.com>
2026-01-08 14:39:20 +0200

    Fix course query to handle undefined courses 2

--- education/education/doctype/program_enrollment/program_enrollment.js
+++ education/education/doctype/program_enrollment/program_enrollment.js
@@ -94,11 +94,12 @@ frappe.ui.form.on('Program Enrollment Course', {
 		frm.fields_dict['courses'].grid.get_field('course').get_query = function(doc) {
 			var course_list = [];
 			if(!doc.__islocal) course_list.push(doc.name);
-			$.each(doc.courses, function(_idx, val) {
+			$.each(doc.courses || [], function(_idx, val) {
 				if (val.course) course_list.push(val.course);
 			});
+			let program_courses = (frm.program_courses || []).map(e => e.course);
 			return { filters: [['Course', 'name', 'not in', course_list],
-				['Course', 'name', 'in', frm.courses.map((e) => e.course)]] };
+				['Course', 'name', 'in', program_courses]] };
 		};
 	}
 });
